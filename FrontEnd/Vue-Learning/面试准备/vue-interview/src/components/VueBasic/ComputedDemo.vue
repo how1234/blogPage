6adb1e654ccfeb2c129ef45a8e223a6da73c9964
Weiying <654033305@qq.com>
2020-06-20 23:29:09 +1000

    vue learning

---
+++ FrontEnd/Vue-Learning/面试准备/vue-interview/src/components/VueBasic/ComputedDemo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+     
+        <p>Num: {{num}}</p>
+        <p>Double Number : {{doubleNum}}</p>
+        <input v-model="doubleNum1" />
+    </div>
+</template>
+
+<script>
+export default {
+    name:"ComputedDemo",
+    data(){
+        return {
+            num:20
+        }
+    },
+    computed:{
+        doubleNum(){
+            return this.num * 2
+        },
+        //v-model里面还有get和set
+        doubleNum1:{
+            get(){
+                return this.num * 2 
+            },
+            set(val){
+                this.num = val / 2
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ FrontEnd/Vue-Learning/面试准备/vue-interview/src/components/VueBasic/WatchDemo.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>{{name}}</p>
+        <p>{{info}}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:"WatchDemo",
+    data(){
+        return {
+            name:"名字",
+            info:{
+                city:"海南"
+            }
+        }
+    },
+    watch:{
+        name(oldVal,val){
+            console.log("name : "+ oldVal + " " + val) //值类型可以拿到oldValue
+        },
+        info:{
+            handler(oldVal,val){
+                console.log("info" + oldVal + " " + val) //引用类型拿不到oldValue
+            },
+            deep:true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
